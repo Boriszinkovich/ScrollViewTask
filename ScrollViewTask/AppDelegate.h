45714c6adc3b4a05e5b44ccb52f18846021f8241
BZ <BZ@bzs-macbook-pro.local>
2016-02-15 16:30:43 +0200

    Initial Commit

---
+++ ScrollViewTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewTask
+//
+//  Created by BZ on 2/15/16.
+//  Copyright © 2016 BZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewTask
+//
+//  Created by BZ on 2/15/16.
+//  Copyright © 2016 BZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
