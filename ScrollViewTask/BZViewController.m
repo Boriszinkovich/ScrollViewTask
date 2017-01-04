c639b7c1a0804906fb76c2c495ce7d8776c4778d
Boris Zinkovich <User@Mac-mini-Boris.local>
2017-01-04 18:11:17 +0200

    fixed layout

--- ScrollViewTask/BZViewController.m
+++ ScrollViewTask/BZViewController.m
@@ -60,156 +60,7 @@
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
-    UIView *theContainerView = [UIView new];
-    [self.view addSubview:theContainerView];
-    
-    theContainerView.theWidth = 400;
-    theContainerView.theHeight = 400;
-    theContainerView.theMinX = 50;
-    theContainerView.theMinY = 100;
-    theContainerView.backgroundColor = [UIColor greenColor];
-    
-    
-    UIView *theTopSeparatorView = theContainerView.theTopSeparatorView;
-    theTopSeparatorView.backgroundColor = [UIColor yellowColor];
-    theTopSeparatorView.theHeight = 20;
-    
-    UIView *theLeftSeparatorView = theContainerView.theLeftSeparatorView;
-    theLeftSeparatorView.backgroundColor = [UIColor blackColor];
-    theLeftSeparatorView.theWidth = 20;
-//    theLeftSeparatorView.theHeight = 40;
-    
-    UIView *theBottomSeparatorView = theContainerView.theBottomSeparatorView;
-    theBottomSeparatorView.backgroundColor = [UIColor redColor];
-    theBottomSeparatorView.theHeight = 70;
-    theBottomSeparatorView.theWidth = 300;
-    UIView *theBottomSeparatorBottomSubview = theBottomSeparatorView.theBottomSeparatorView;
-    {
-        theBottomSeparatorBottomSubview.theHeight = 20;
-        theBottomSeparatorBottomSubview.theWidth = 40;
-        theBottomSeparatorBottomSubview.backgroundColor = [UIColor blueColor];
-    }
-    
-    UIView *theRightSeparatorView = theContainerView.theRightSeparatorView;
-    theRightSeparatorView.backgroundColor = [UIColor purpleColor];
-    theRightSeparatorView.theWidth = 50;
-    theRightSeparatorView.theHeight = 100;
-    
-//    theBottomSeparatorView.theMinX = 70;
-//    theLeftSeparatorView.theCenterX = 30;
-//    theRightSeparatorView.theMaxX = 20;
-//    theBottomSeparatorLeftSubview.theMaxX = 100;
-    NSLog(@"%f",theBottomSeparatorBottomSubview.theWidth);
-    [UIView animateWithDuration:20
-                          delay:3
-                        options:UIViewAnimationOptionCurveEaseInOut
-                     animations:^
-     {
-         theContainerView.theWidth -= theContainerView.theWidth/2;
-         theContainerView.theHeight -= theContainerView.theHeight/2;
-         theRightSeparatorView.theMinX = 0;
-     }
-                     completion:^(BOOL finished)
-     {
-         NSLog(@"%f",theBottomSeparatorBottomSubview.theWidth);
-         NSLog(@"%f",theBottomSeparatorBottomSubview.theHeight);
-         NSLog(@"%f",theRightSeparatorView.theHeight);
-     }];
-//
-//////    [BZExtensionsManager methodDispatchAfterSeconds:3 withBlock:^
-//////     {
-//////         theBottomSeparatorView.theMinY = 200;
-//////     }];
-////    
-////    [BZExtensionsManager methodDispatchAfterSeconds:6 withBlock:^
-////     {
-//////         theContainerView.theWidth = 200;
-////         theContainerView.theHeight = 500;
-//////         theBottomSeparatorView.theHeight = 100;
-////     }];
-//    
-//    
-//    [BZExtensionsManager methodDispatchAfterSeconds:3 withBlock:^
-//     {
-//         [UIView animateWithDuration:5
-//                          animations:^
-//          {
-//              theBottomSeparatorView.theMinX = 0;
-////              theContainerView.theWidth = 800;
-//          }];
-//         
-//     }];
-//    [BZExtensionsManager methodDispatchAfterSeconds:12 withBlock:^
-//     {
-////         [theBottomSeparatorView removeFromSuperview];
-//         [UIView animateWithDuration:5
-//                          animations:^
-//          {
-//              theContainerView.theWidth = 800;
-////              theBottomSeparatorView.theHeight = 100;
-////              theBottomSeparatorView.theWidth = 20;
-//          }];
-//         
-//     }];
-    
 }
-
-
-//- (void)viewDidAppear:(BOOL)animated
-//{
-//    [super viewDidAppear:animated];
-//    if (self.isFirstLoad)
-//    {
-//        self.isFirstLoad = NO;
-//    }
-//    BZAnimation *theBZAnimation = [BZAnimation new];
-//    theBZAnimation.theDuration = 0.6;
-//    UILabel *theExampleLabel = [UILabel new];
-//    theExampleLabel.theWidth = 100;
-//    theExampleLabel.theHeight = 100;
-//    theExampleLabel.theMinX = 100;
-//    theExampleLabel.theMinY = 100;
-//    theExampleLabel.backgroundColor = [UIColor blueColor];
-//    
-//    UIView *theContainerView = [UIView new];
-//    theContainerView.theMinY = 100;
-//    theContainerView.theMinX = 100;
-//    theContainerView.theWidth = 700;
-//    theContainerView.theHeight = 700;
-//    theContainerView.backgroundColor = [UIColor yellowColor];
-//    [self.view addSubview:theContainerView];
-//    [theContainerView addSubview:theExampleLabel];
-//    theBZAnimation.theOptions = UIViewAnimationOptionTransitionFlipFromLeft;
-//    theBZAnimation.theView = self.view;
-//    UIView *theSecondView = [UIView new];
-//    theSecondView.theMinX = 400;
-//    theSecondView.theMinY = 400;
-//    theSecondView.theHeight = 300;
-//    theSecondView.theWidth = 300;
-//    theSecondView.backgroundColor = [UIColor greenColor];
-//    [theBZAnimation methodSetAnimationBlock:^
-//    {
-//        [theExampleLabel removeFromSuperview];
-//        [theContainerView addSubview:theSecondView];
-//    }];
-//    [theBZAnimation methodSetCompletionBlock:^(BOOL theIsAnimationComplited)
-//    {
-//        NSLog(@"completed");
-//    }];
-//
-//    [self.view addSubview:theExampleLabel];
-////    theBZAnimation.theOptions = UIViewAnimationCurveLinear;
-////    [theBZAnimation methodSetAnimationBlock:^{
-////        theExampleLabel.theMinY = 300;
-////        theExampleLabel.theMinX = 300;
-////    }];
-////    [theBZAnimation methodSetCompletionBlock:^(BOOL theIsAnimationComplited)
-////     {
-////         NSLog(@"completed");
-////     }];
-//    
-//    [theBZAnimation methodStart];
-//}
 
 - (void)viewWillDisappear:(BOOL)animated
 {
@@ -232,7 +83,6 @@
     self.isFirstLoad = NO;
     self.navigationController.navigationBar.hidden = YES;
     self.automaticallyAdjustsScrollViewInsets = NO;
-    return;
 //    UIView *theContainerView = [UIView new];
 //    [self.view addSubview:theContainerView];
 //    
@@ -272,35 +122,13 @@
     
     
     
-    UIView *theRedView = [UIView new];
-    theRedView.theWidth = self.view.theWidth;
-    theRedView.theHeight = self.view.theHeight;
-    theRedView.backgroundColor = [UIColor redColor];
-    theRedView.tag = 3;
-    [theMainBZPageView methodAddPage:theRedView];
-    
-    
-    UIView *theGreenView = [UIView new];
-    theGreenView.theWidth = self.view.theWidth;
-    theGreenView.theHeight = self.view.theHeight;
-    theGreenView.backgroundColor = [UIColor greenColor];
-    theGreenView.tag = 6;
-    [theMainBZPageView methodAddPage:theGreenView];
-
-    
-    UIView *theBlueView = [UIView new];
-    theBlueView.theWidth = self.view.theWidth;
-    theBlueView.theHeight = self.view.theHeight;
-    theBlueView.backgroundColor = [UIColor blueColor];
-    theBlueView.tag = 9;
-    [theMainBZPageView methodAddPage:theBlueView];
-//    for (int i = 0; i < theLayoutsDictsArray.count; i++)
-//    {
-//        BZScrollContentUIView *theBZScrollContentUIView = [BZScrollContentUIView new];
-//        theBZScrollContentUIView.theLayoutDictionary = theLayoutsDictsArray[i];
-//        [theMainBZPageView methodAddPage:theBZScrollContentUIView];
-//        
-//    }
+    for (int i = 0; i < theLayoutsDictsArray.count; i++)
+    {
+        BZScrollContentUIView *theBZScrollContentUIView = [BZScrollContentUIView new];
+        theBZScrollContentUIView.theLayoutDictionary = theLayoutsDictsArray[i];
+        [theMainBZPageView methodAddPage:theBZScrollContentUIView];
+        
+    }
     theMainBZPageView.theDelegate = self;
     
     SMPageControl *theMainSMPageControl = [SMPageControl new];
